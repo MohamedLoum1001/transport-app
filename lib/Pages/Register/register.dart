dd7094cbf7c36a6f91345012aa4575b3ce29295d
MohamedLoum1001 <mloum137@gmail.com>
2024-11-14 15:53:29 +0100

    logique inscription

--- lib/Pages/Register/register.dart
+++ lib/Pages/Register/register.dart
@@ -1,5 +1,8 @@
 // screens/register_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Import nécessaire pour TextInputFormatter
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -10,30 +13,43 @@ class _RegisterState extends State<Register> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController givenNameController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String selectedUserType = 'Client'; // Définit 'Client' par défaut
 
-  void registerUser() {
-    if (passwordController.text != confirmPasswordController.text) {
-      showErrorDialog("Les mots de passe ne correspondent pas");
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text("Inscription réussie"),
-          content: Text("Bienvenue, $selectedUserType!"),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-                Navigator.pushReplacementNamed(context, '/');
-              },
-              child: Text("OK"),
-            ),
-          ],
-        ),
+  // État pour afficher ou masquer le mot de passe
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> registerUser() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
       );
+
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'nom': nameController.text,
+        'prenom': givenNameController.text,
+        'telephone': phoneController.text,
+        'email': emailController.text,
+        'type': selectedUserType,
+        'uid': userCredential.user!.uid,
+      });
+
+      showSuccessDialog("Inscription réussie! Bienvenue, $selectedUserType!");
+
+    } catch (e) {
+      showErrorDialog("Erreur d'inscription : ${e.toString()}");
     }
   }
 
@@ -53,38 +69,180 @@ class _RegisterState extends State<Register> {
     );
   }
 
+  void showSuccessDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Succès"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, '/');
+            },
+            child: Text("OK"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Inscription')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Nom')),
-            TextField(controller: givenNameController, decoration: InputDecoration(labelText: 'Prénom')),
-            TextField(controller: phoneController, decoration: InputDecoration(labelText: 'Téléphone')),
-            TextField(controller: passwordController, decoration: InputDecoration(labelText: 'Mot de passe'), obscureText: true),
-            TextField(controller: confirmPasswordController, decoration: InputDecoration(labelText: 'Confirmer mot de passe'), obscureText: true),
-            DropdownButtonFormField<String>(
-              value: selectedUserType,
-              items: [
-                DropdownMenuItem(value: 'Client', child: Text('Client')),
-                DropdownMenuItem(value: 'Transporteur', child: Text('Transporteur')),
-              ],
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedUserType = newValue!;
-                });
-              },
-              decoration: InputDecoration(labelText: 'Type de profil'),
-            ),
-            ElevatedButton(
-              onPressed: registerUser,
-              child: Text('S\'inscrire'),
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: 'Nom',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre nom';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: givenNameController,
+                decoration: InputDecoration(
+                  labelText: 'Prénom',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre prénom';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: phoneController,
+                decoration: InputDecoration(
+                  labelText: 'Téléphone',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(10),
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre numéro de téléphone';
+                  }
+                  if (value.length != 10) {
+                    return 'Le numéro de téléphone doit contenir 10 chiffres';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer votre adresse e-mail';
+                  }
+                  if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                    return 'Veuillez entrer une adresse e-mail valide';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Mot de passe',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !_isPasswordVisible,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez entrer un mot de passe';
+                  }
+                  if (value.length < 6) {
+                    return 'Le mot de passe doit contenir au moins 6 caractères';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: 'Confirmer mot de passe',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !_isConfirmPasswordVisible,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Veuillez confirmer votre mot de passe';
+                  }
+                  if (value != passwordController.text) {
+                    return 'Les mots de passe ne correspondent pas';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              DropdownButtonFormField<String>(
+                value: selectedUserType,
+                items: [
+                  DropdownMenuItem(value: 'Client', child: Text('Client')),
+                  DropdownMenuItem(value: 'Transporteur', child: Text('Transporteur')),
+                ],
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedUserType = newValue!;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Type de profil'),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: registerUser,
+                child: Text('S\'inscrire'),
+              ),
+            ],
+          ),
         ),
       ),
     );
